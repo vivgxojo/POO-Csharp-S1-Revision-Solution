29972085de58f654ce74e1ae532db4a57fea0a8f
Maryse M <Maryse.Mongeau@cegepoutaouais.qc.ca>
2025-01-09 11:49:30 -0500

    Dépôt solution

---
+++ S1_Revision/Fonctions.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S1_Revision
+{
+    /// <summary>
+    /// C'est une classe avec des fonction écrites par ChatGPT
+    /// </summary>
+    public class Fonctions
+    {
+
+        /// <summary>
+        /// Cette fonction reçoit une chaîne de caractères en argument et retourne sa longueur.
+        /// </summary>
+        /// <param name="chaine">La chaîne de caractères à évaluer.</param>
+        /// <returns>La longueur de la chaîne.</returns>
+        public static int Longueur(string chaine)
+        {
+            return chaine.Length;
+        }
+
+        /// <summary>
+        /// Cette fonction vérifie si un nombre entier est un multiple de 10.
+        /// </summary>
+        /// <param name="nombre">Le nombre entier à vérifier.</param>
+        /// <returns>True si le nombre est un multiple de 10, sinon False.</returns>
+        public static bool EstMultiple10(int nombre)
+        {
+            return nombre % 10 == 0;
+        }
+
+        /// <summary>
+        /// Cette fonction calcule le complément d'un nombre entier pour atteindre le multiple de 10 le plus proche.
+        /// </summary>
+        /// <param name="nombre">Le nombre entier à compléter.</param>
+        /// <returns>La valeur manquante pour atteindre le multiple de 10 suivant.</returns>
+        public static int Complement10(int nombre)
+        {
+            return (10 - (nombre % 10)) % 10;
+        }
+
+        /// <summary>
+        /// Cette fonction retourne le chiffre des dizaines d'un nombre entier.
+        /// </summary>
+        /// <param name="nombre">Le nombre entier dont on veut extraire le chiffre des dizaines.</param>
+        /// <returns>Le chiffre des dizaines.</returns>
+        public static int Dizaine(int nombre)
+        {
+            return Math.Abs(nombre) / 10 % 10;
+        }
+
+        /// <summary>
+        /// Cette fonction retourne le chiffre des centaines d'un nombre entier.
+        /// </summary>
+        /// <param name="nombre">Le nombre entier dont on veut extraire le chiffre des centaines.</param>
+        /// <returns>Le chiffre des centaines.</returns>
+        public static int Centaine(int nombre)
+        {
+            return Math.Abs(nombre) / 100 % 10;
+        }
+
+    }
+}
---
+++ S1_Revision/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+using S1_Revision;
+
+// Collections de valeurs à tester
+string[] chaines = { "Bonjour", "", "a", "maman", "MAMAN", "A", "ma maman", "1", "123", "Password_123" };
+int[] nombres = { 10, 0, 9, 11, -1, 20, 99, 100, 200};
+
+// Collection pour stocker les résultats
+int[] resultatsLongueur = new int[chaines.Length];
+bool[] resultatsEstMultiple10 = new bool[nombres.Length];
+int[] resultatsComplement10 = new int[nombres.Length];
+int[] resultatsDizaine = new int[nombres.Length];
+int[] resultatsCentaine = new int[nombres.Length];
+
+// Tableau 2D pour les résultats des fonctions qui ont le même type de retour et les mêmes tests
+int[][] tab2D = { resultatsComplement10, resultatsDizaine, resultatsCentaine };
+
+// Boucles qui exécutent les tests et stockent les résultats
+for (int i = 0; i < chaines.Length; i++)
+{
+    resultatsLongueur[i] = Fonctions.Longueur(chaines[i]);
+}
+
+for (int i = 0; i < nombres.Length; i++)
+{
+    resultatsEstMultiple10[i] = Fonctions.EstMultiple10(nombres[i]);
+    tab2D[0][i] = Fonctions.Complement10(nombres[i]);
+    tab2D[1][i] = Fonctions.Dizaine(nombres[i]);
+    tab2D[2][i] = Fonctions.Centaine(nombres[i]);
+}
+
+// Boucles pour afficher tous les essais et leurs résultats
+Console.WriteLine("\nTests de Longueur :");
+for (int i = 0; i < chaines.Length; i++)
+{
+    Console.WriteLine($"La longueur de : \"{chaines[i]}\" est : {resultatsLongueur[i]}");
+}
+Console.WriteLine("\nTests de Multiples de 10 :");
+for (int i = 0; i < nombres.Length; i++)
+{
+    Console.WriteLine($"La longueur de : \"{nombres[i]}\" est : {resultatsEstMultiple10[i]}");
+}
+for (int i = 0; i < 3; i++)
+{
+    switch (i)
+    {
+        case 0:
+            Console.WriteLine("\nTests de Complement 10 :");
+            break;
+        case 1:
+            Console.WriteLine("\nTests de Dizaines :");
+            break;
+        case 2:
+            Console.WriteLine("\nTests de Centaines :");
+            break;
+    }
+
+    for (int j = 0; j < nombres.Length; j++)
+    {
+        Console.WriteLine($"Test : \"{nombres[j]}\" => Résultat : {tab2D[i][j]}");
+    }
+}
